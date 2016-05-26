f9bed25c069ca2c7f361d9f26999b73db7c684c4
Mark Watson <mkw5053@gmail.com>
2016-05-26 13:37:39 -0700

    content-length

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-gunzip-middleware "0.1.1"
+(defproject ring-gunzip-middleware "0.1.2"
   :description "Ring middleware for handling compressed POST/PUT body"
   :url "https://github.com/mkwatson/ring-gunzip-middleware"
   :license {:name "Eclipse Public License"
--- src/ring_gunzip_middleware/core.clj
+++ src/ring_gunzip_middleware/core.clj
@@ -1,14 +1,11 @@
 (ns ring-gunzip-middleware.core
   (:require [clojure.string :refer [lower-case]]))
 
-(def lower-str (comp lower-case name))
-
 (defn wrap-gunzip [handler]
-  (fn [{:keys [body headers] :as request}]
-    (if-let [content-type (some->> headers
-                                   (map (fn [[k v]] [(lower-str k) v]))
-                                   (get "content-encoding")
-                                   lower-str
-                                   #{"gzip"})]
-      (handler (update request :body #(java.util.zip.GZIPInputStream. %)))
-      (handler request))))
+  (fn [{headers :headers :as request}]
+    (let [{content-encoding "content-encoding"
+           content-length "content-length"} headers]
+      (if (and content-length
+               (#{"gzip"} content-encoding))
+        (handler (update request :body #(java.util.zip.GZIPInputStream. %)))
+        (handler request)))))
